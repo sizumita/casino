ba3a3f391d96e17d5e629a729170499354cd5909
Sumito Izumita <sumito@izumita.com>
2020-05-05 22:04:59 +0900

    Add main.py
    [Add] メイン処理

---
+++ main.py
@@ -0,0 +1,13 @@
+from bot import CasinoBot
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+bot = CasinoBot()
+
+
+bot.run(os.environ.get("TOKEN"))
