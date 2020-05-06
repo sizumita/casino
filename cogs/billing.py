118feb5ad4e022c62b3ed94686bad1a2861e3c34
Sumito Izumita <sumito@izumita.com>
2020-05-06 14:01:51 +0900

    [Add] 金額確認コマンド [Add] 登録コマンド [Add] お金を渡すコマンド

--- cogs/billing.py
+++ cogs/billing.py
@@ -1,13 +1,41 @@
 from discord.ext import commands
+import discord
 
 
 class Billing(commands.Cog):
     """
-    お金の確認など
+    お金の確認、お金の受け渡し、登録など
     """
-
     def __init__(self, bot):
         self.bot = bot
+
+    async def cog_check(self, ctx):
+        if ctx.author.id not in self.bot.users.keys():
+            return False
+
+    @commands.command(aliases=['money', 'balance'])
+    async def bal(self, ctx):
+        """所持金を確認します。"""
+
+    @commands.command()
+    async def pay(self, ctx, to: discord.User, money: int):
+        """指定したユーザーにお金を渡します。"""
+        if to.id not in self.bot.users.keys():
+            await ctx.send(f'ユーザー:{to.mention}はゲームに登録していません。')
+            return
+        
+        if money > self.bot.users[ctx.author.id]:
+            await ctx.send('指定された金額はあなたの所持金をオーバーしています。')
+            return
+
+    @commands.command()
+    async def register(self, ctx):
+        """ユーザー登録をします。"""
+        if ctx.author.id in self.bot.users.keys():
+            await ctx.send('既に登録されています。')
+            return
+        self.bot.users[ctx.author.id] = 10000
+        await ctx.send('登録されました。')        
 
 
 def setup(bot):
