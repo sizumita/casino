0d397f88b8df9648770932eb7ad9fd4d1feb2423
Sumito Izumita <sumito@izumita.com>
2020-05-06 18:18:30 +0900

    処理追加
    [Add] フィルター

--- cogs/high_low.py
+++ cogs/high_low.py
@@ -8,8 +8,20 @@ class HighLow(commands.Cog):
 
     @commands.command(aliases=['hal', 'hl'])
     async def high_and_low(self, ctx, bid):
+        if bid > self.bot.users[ctx.author.id]:
+            await ctx.send('指定された金額はあなたの所持金をオーバーしています。')
+            return
+        if ctx.author.id in self.bot.game_que:
+            await ctx.send('あなたはすでにゲームを開始しています。')
+            return
+        self.bot.users[ctx.author.id] -= bid
+        self.bot.game_que.append(ctx.author.id)
+
         game = HighAndLow(self.bot, ctx, bid)
         result = await game.play()
+        
+        self.bot.users[ctx.author.id] += bid
+        self.bot.game_que.remove(ctx.author.id)
 
 
 def setup(bot):
