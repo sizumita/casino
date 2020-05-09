d0bcfb1422cd0fedcf49eaddb081b94a61ba9253
Sumito Izumita <sumito@izumita.com>
2020-05-09 14:53:56 +0900

    お金は全部使い切るように

--- cogs/billing.py
+++ cogs/billing.py
@@ -42,7 +42,7 @@ class Billing(commands.Cog):
             await ctx.send(f'ユーザー:{to.mention}はゲームに登録していません。')
             return
 
-        if money > self.bot.players[ctx.author.id] and ctx.author.id != 212513828641046529:
+        if money >= self.bot.players[ctx.author.id] and ctx.author.id != 212513828641046529:
             await ctx.send('指定されたnyan額はあなたの所持金をオーバーしています。')
             return
 
@@ -52,6 +52,9 @@ class Billing(commands.Cog):
 
         if ctx.author.id != 212513828641046529:
             self.bot.players[ctx.author.id] -= money
+        if ctx.author.id == 212513828641046529 and money >= self.bot.players[ctx.author.id]:
+            self.bot.players[ctx.author.id] -= money
+
         self.bot.players[to.id] += money
         await ctx.send(f'{to.mention}さんに{money}nyanを送りました。')
 
